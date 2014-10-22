7d3d6fca25bf84631f4289ce08f2397b98b4c679
Jeff Marshall <jeff@classy.co>
2014-10-22 05:35:37 -0700

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,3 @@
+FROM pandeiro/lein
+EXPOSE 3000
+CMD ring server-headless
---
+++ src/causemap_parse/handler.clj
@@ -0,0 +1,21 @@
+(ns causemap-parse.handler
+  (:require [compojure.core :refer :all]
+            [compojure.handler :as handler]
+            [compojure.route :as route]
+            [ring.util.response :refer [resource-response response]]
+            [ring.middleware.json :as middleware]
+            [duckling.core :as p]))
+
+(p/load!) ;; Load default configuration
+
+(defroutes app-routes
+  (GET "/" [] (response (p/parse :en$core ;; core configuration for English ; see also :fr$core, :es$core, :cn$core
+         (get-in request [:body "period"])
+         [:time])))
+  (route/resources "/")
+  (route/not-found "Not Found"))
+
+(def app
+  (-> (handler/site app-routes)
+      (middleware/wrap-json-body)
+      (middleware/wrap-json-response)))
