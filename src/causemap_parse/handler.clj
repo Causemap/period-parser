4c6ed2c27bdde4a58cd198d9b2ecc4a40c0d4727
Jeff Marshall <jeff@classy.co>
2014-10-22 06:21:15 -0700

    syntax error

--- src/causemap_parse/handler.clj
+++ src/causemap_parse/handler.clj
@@ -9,13 +9,13 @@
 (p/load!) ;; Load default configuration
 
 (defroutes app-routes
-  (POST "/" [request] (response (p/parse :en$core ;; core configuration for English ; see also :fr$core, :es$core, :cn$core
-         (get-in request [:body "period"])
+  (POST "/" request (response (p/parse :en$core ;; core configuration for English ; see also :fr$core, :es$core, :cn$core
+         (get-in request [:body :period])
          [:time])))
   (route/resources "/")
   (route/not-found "Not Found"))
 
 (def app
   (-> (handler/site app-routes)
-      (middleware/wrap-json-body)
+      (middleware/wrap-json-body {:keywords? true})
       (middleware/wrap-json-response)))
