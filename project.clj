5d74a477de8f4e8d0db8ca671ef2acd019df240f
Jeff Marshall <jeff@classy.co>
2014-10-30 22:34:34 -0700

    adding cors

--- project.clj
+++ project.clj
@@ -5,6 +5,7 @@
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [wit/duckling "0.2.2"]
                  [compojure "1.1.9"]
+                 [ring-cors "0.1.4"]
                  [ring/ring-core "1.2.0"]
                  [ring/ring-json "0.2.0"]]
   :plugins [[lein-ring "0.8.12"]]
--- src/causemap_parse/handler.clj
+++ src/causemap_parse/handler.clj
@@ -2,6 +2,7 @@
   (:require [compojure.core :refer :all]
             [compojure.handler :as handler]
             [compojure.route :as route]
+            [ring.middleware.cors :refer [wrap-cors]]
             [ring.util.response :refer [resource-response response]]
             [ring.middleware.json :as middleware]
             [duckling.core :as p]))
@@ -17,5 +18,7 @@
 
 (def app
   (-> (handler/site app-routes)
+      (wrap-cors my-routes :access-control-allow-origin #"http://www.causemap.org"
+        :access-control-allow-methods [:post])
       (middleware/wrap-json-body {:keywords? true})
       (middleware/wrap-json-response)))
